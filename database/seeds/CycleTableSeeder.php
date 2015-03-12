8cfbd69df66da03965b689a6aed48ca007b5e2d4
Eyal L <elpd.dev@runbox.com>
2015-03-13 01:03:15 +0200

    added cycle seed for test

--- database/seeds/CycleTableSeeder.php
+++ database/seeds/CycleTableSeeder.php
@@ -8,15 +8,26 @@ class CycleTableSeeder extends Seeder
     public function run ()
     {
         DB::table('cycles')->delete();
-                
-        $newItem = Cycle::create(
-                array(
-                        'startDate' => '2015-01-01'
-                ));
-        
-        $errors = $newItem->getErrors();
-        if (count($errors) > 0) {
+
+        $itemsData = array(
+          array(
+            'startDate' => '2015-01-01',
+            'num' => 1
+          ),
+          array(
+            'startDate' => '2015-02-01',
+            'num' => 1
+          )
+        );
+
+        foreach ($itemsData as $itemData) {
+
+          $newItem = Cycle::create($itemData);
+
+          $errors = $newItem->getErrors();
+          if (count($errors) > 0) {
             throw new Exception($errors);
-        }
+          }
+        };
     }
 }
