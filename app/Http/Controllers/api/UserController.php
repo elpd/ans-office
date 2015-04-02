e0c9a976a2341835d52607eb82d8ebf9fb7aa863
Eyal L <elpd.dev@runbox.com>
2015-04-02 15:04:03 +0300

    fix user add password confirmation. todo in full in additional feature

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -6,20 +6,26 @@ use Request;
 use App\User;
 use Hash;
 
-class UserController extends Controller {
+class UserController extends Controller
+{
 
-	protected $biClass = 'App\User';
+    protected $biClass = 'App\User';
 
-	use GeneralRestControlling {
-		store as generalStore;
-	}
+    use GeneralRestControlling {
+        store as generalStore;
+    }
 
-	public function store() {
-		$password = Request::get('password');
-		$hashedPassword = Hash::make($password);
+    public function store()
+    {
+        $password = Request::get('password');
+        $hashedPassword = Hash::make($password);
+        $password_confirmation = $hashedPassword; // TODO: f#42
 
-		Request::merge(['password' => $hashedPassword]);
+        Request::merge([
+            'password' => $hashedPassword,
+            'password_confirmation' => $password_confirmation
+        ]);
 
-		return $this->generalStore();
-	}
+        return $this->generalStore();
+    }
 }
--- app/User.php
+++ app/User.php
@@ -9,43 +9,48 @@ use Watson\Validating\ValidatingTrait;
 use Bican\Roles\Contracts\HasRoleAndPermissionContract;
 use Bican\Roles\Traits\HasRoleAndPermission;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+{
 
-	use Authenticatable, CanResetPassword;
+    use Authenticatable, CanResetPassword;
     use GeneralModel;
 
-	use ValidatingTrait;
+    use ValidatingTrait;
 
-	use HasRoleAndPermission;
+    use HasRoleAndPermission;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	public $fillable = ['name', 'email', 'password'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public $fillable = ['name',
+        'email',
+        'password',
+        'password_confirmation'
+    ];
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = ['password', 'remember_token'];
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
 
-	protected $rules = [
+    protected $rules = [
         'password' => [
             'confirmed'
         ]
     ];
 
-	public $nullable = [];
+    public $nullable = [];
 
     public function settings()
     {
